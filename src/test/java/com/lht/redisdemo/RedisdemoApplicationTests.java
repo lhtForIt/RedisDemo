2ae58a1d30bab3168ebc718a178a4fa49584bc69
lianght1 <lianght1@lenovo.com>
2023-05-30 17:45:14 +0800

    init

---
+++ src/test/java/com/lht/redisdemo/RedisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lht.redisdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
